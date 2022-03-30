e4525799eea2d4ca1a940cf06b5f4958f6835e00
ADarkHero <maik.riedlsperger@gmail.com>
2022-03-30 16:01:32 +0200

    Fixed upload for multiple articles in a shipment

--- categoryWizard.php
+++ categoryWizard.php
@@ -1,4 +1,12 @@
+
+<head>
+    <meta charset="Windows-1252">
+</head>
+
+
 <?php
+    //Both lists must have the same encoding
+    //categories.csv will always be ansi encoded
     $categoryList = "inc/categories.csv";
     $textList = "inc/texts.csv";
 
@@ -11,18 +19,20 @@
         //Only add, if the category has no required features
         if(!isset($data[1])){
             //Categories are saved in ANSI
-            array_push($categories, convertANSItoUTF8($data[0]));
+            array_push($categories, $data[0]);
         } 
     }
+
+    //var_dump($categories);
 
     //Read all texts
     $handle = fopen($textList, "r");
     while (($data = fgetcsv($handle, 0, ";")) !== FALSE) {
         foreach($categories as &$value){
-            //List is ansi encoded
-            if(strpos(convertANSItoUTF8($data[0]), $value) !== false){
+            //List is ANSI encoded; Value is ANSI
+            if(strpos(strtolower($data[0]), strtolower($value)) !== false){
                 $csv .= $data[1] . ';' . $value . ';' . $data[0] . PHP_EOL;
-                echo $data[1] . ';' . $value . ';' . convertANSItoUTF8($data[0]) . "<br>";
+                echo $data[1] . ';' . convertANSItoUTF8($value) . ';' . convertANSItoUTF8($data[0]) . "<br>";
             }
         }
     }
@@ -31,6 +41,10 @@
     $fp = fopen('inc/categoryWizard.csv', 'w');
     fwrite($fp, $csv);
     fclose($fp);
+
+
+
+
 
 
     function convertANSItoUTF8($str) {
